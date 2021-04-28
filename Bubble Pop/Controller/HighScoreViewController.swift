6adfb294b0e1b328c79f41d4dd10234d1b97dfdb
lachlan <lachlan@lachlanb.com>
2021-04-27 22:52:01 -0700

    Added high score table

--- Bubble Pop/Controller/HighScoreViewController.swift
+++ Bubble Pop/Controller/HighScoreViewController.swift
@@ -7,12 +7,42 @@
 
 import UIKit
 
-class HighScoreViewController: UIViewController {
+class HighScoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var highscoreTableView: UITableView!
+    var highscores: [[String]] = [[]]
+    
+    let names: [String] = ["123", "456"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        highscoreTableView.delegate = self
+        highscoreTableView.dataSource = self
+        
+        
+        if let highscores =  UserDefaults.standard.value(forKey: "highscores") as? [[String]] {
+            self.highscores = highscores
+        }
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("The row is tapped")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("Number of scores: \(highscores.count)")
+        print(highscores)
+        return highscores.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let customCell = highscoreTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
+        print(indexPath)
+        customCell.textLabel?.text = ("Name: \(highscores[indexPath.row][0]). Score: \(highscores[indexPath.row][1]).")
+        customCell.backgroundColor = .blue
+        return customCell
     }
     
 
